d2dfe0cf316ff55670c450aad21e2c01e8b57b7f
parulloyal <139440794+parulloyal@users.noreply.github.com>
2024-03-18 18:28:40 +1100

    initial_git commit

---
+++ sit707/src/main/java/selenium_practice/sit707/App.java
@@ -0,0 +1,13 @@
+package selenium_practice.sit707;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
